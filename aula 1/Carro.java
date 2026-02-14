aa0b7939695a9055accd64e8f156eb0543d99775
Paulo <pcrrp.pereira@gmail.com>
2026-02-13 21:33:11 -0300

    Primeiro exercicio

---
+++ aula 1/Carro.java
@@ -0,0 +1,56 @@
+public class Carro{
+       
+    String modelo;
+    String marca;       
+    int ano;
+    String cor;    
+    int tanque = 100;
+    double potencia;
+
+    
+
+    Carro(String _modelo, 
+        String _marca, 
+        String _cor,
+        int _ano, 
+        double _potencia )
+        
+        {
+        
+        modelo = _modelo;
+        marca = _marca;
+        ano = _ano;
+        cor = _cor;
+        potencia = _potencia;
+    }
+              
+        public void mostrarInfo(String modelo){
+           System.out.println("Modelo = " + modelo);
+           System.out.println("Marca = " + marca);
+           System.out.println("Ano = " + ano);
+           System.out.println("Cor = " + cor);
+           System.out.println("Potencia = " + potencia);
+
+        }
+
+        public void mostraTanque(){
+            System.out.println("Tanque = " + tanque + "%");
+        }
+        
+        public double anda(int tanque){
+
+            if (tanque > 9){
+                tanque -= 10;
+                System.out.println("O " + modelo + "está em movimento"); 
+            }
+
+            else {
+                System.out.println("Combustivel insuficiente");
+            }
+                        return tanque;
+
+
+        }
+
+    }
+
---
+++ aula 1/TesteAula01.java
@@ -0,0 +1,15 @@
+public class TesteAula01 {
+    
+    public static void main(String[] args) {
+        Carro c1 = new Carro("Fusca", "Wolks Wagen", "Azul", 1973, 1.0);
+        Carro c2 = new Carro("Corsa", "Chevrolet", "vermelho", 2001, 1.0);
+        Carro c3 = new Carro("Civic", "Honda", "Cinza", 2001, 1.8);
+        
+        c1.mostrarInfo(null);
+        c2.mostrarInfo(null);
+        c3.mostrarInfo(null);
+
+        c1.anda();
+    }
+
+}
